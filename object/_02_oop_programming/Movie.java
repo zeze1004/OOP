81bfbec3ac58865667f59ea18a09166bfcce8056
zeze <thwjd2717@gmail.com>
2023-11-26 19:06:59 +0900

    feat: 영화 정보를 구현

---
+++ object/_02_oop_programming/Movie.java
@@ -0,0 +1,25 @@
+package object._02_oop_programming;
+
+import java.time.Duration;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;   // 하나의 영화에 대해 단 하나의 할인 정책만 설정되므로 할인정책 인스턴스 하나만 생성자로 강제
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
+}
